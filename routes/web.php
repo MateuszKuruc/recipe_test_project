b2629879c9fc98658f010384d1b155a2a4a3531b
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-06-16 12:25:36 +0200

    add new data handling to routes

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,16 @@
 <?php
 
+use App\Models\Category;
 use App\Models\Recipe;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     $carouselRecipes = Recipe::orderBy('view_count', 'DESC')->take(3)->get();
     $latestRecipes = Recipe::latest()->take(4)->get();
+    $categories = Category::withCount('recipes')
+        ->orderBy('recipes_count', 'DESC')
+        ->take(3)
+        ->get();
 
-    return view('home', [
-        'carouselRecipes' => $carouselRecipes,
-        'latestRecipes' => $latestRecipes
-    ]);
+    return view('home', compact('carouselRecipes', 'latestRecipes', 'categories'));
 });
