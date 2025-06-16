b3f3512199b55012004d6dbeeca7c1c952c72cb2
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-06-16 22:16:59 +0200

    add route for single recipe

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\RecipeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
@@ -9,3 +10,5 @@ Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
 
 Route::get('/categories/{category}', [CategoryController::class, 'show'])->name('categories.show');
+
+Route::get('/recipes/{recipe}', [RecipeController::class, 'show'])->name('recipes.show');
