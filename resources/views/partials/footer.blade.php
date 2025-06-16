2cccf56a7b2e579df18e05e8bc975a18c7348bd5
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-06-16 07:19:46 +0200

    add header and footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,117 @@
+<footer class="mc-main pt-5 mt-5 border-top">
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-5">
+        <div class="col">
+            <a href="#!"
+               class="d-flex align-items-center mb-3 link-body-emphasis text-decoration-none site-logo">
+                <div class="mc-site-header">
+                    <img src="img/logo.png" alt="Logo" width="70px">
+                    <h3 class="text-center site-title">
+                        Lara<span class="fw-light">Recipes</span>
+                    </h3>
+                </div>
+            </a>
+            <p class="text-body-secondary">
+                Laravel Tutorial by <a href="https://x.com/mkwsra" target="_blank">
+                    Mo Kawsara
+                </a>
+            </p>
+        </div>
+
+        <div class="col mb-3">
+
+        </div>
+
+        <div class="col mb-3">
+            <h5>Categories</h5>
+            <ul class="nav flex-column">
+                <li class="nav-item mb-2">
+                    <a href="#" class="nav-link p-0 text-body-secondary">
+                        Category 1 <small>(12 recipes)</small>
+                    </a>
+                </li>
+                <li class="nav-item mb-2">
+                    <a href="#" class="nav-link p-0 text-body-secondary">
+                        Category 2 <small>(7 recipes)</small>
+                    </a>
+                </li>
+                <li class="nav-item mb-2">
+                    <a href="#" class="nav-link p-0 text-body-secondary">
+                        Category 3 <small>(6 recipes)</small>
+                    </a>
+                </li>
+                <li class="nav-item mb-2">
+                    <a href="#" class="nav-link p-0 text-body-secondary">
+                        Category 4 <small>(6 recipes)</small>
+                    </a>
+                </li>
+                <li class="nav-item mb-2">
+                    <a href="#" class="nav-link p-0 text-body-secondary">
+                        Category 5 <small>(4 recipes)</small>
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="col mb-3">
+            <h5>Featured Recipes</h5>
+            <ul class="nav flex-column">
+                <li class="nav-item mb-2">
+                    <a href="#" class="nav-link p-0 text-body-secondary">
+                        Recipe title 1
+                    </a>
+                </li>
+                <li class="nav-item mb-2">
+                    <a href="#" class="nav-link p-0 text-body-secondary">
+                        Recipe title 2
+                    </a>
+                </li>
+                <li class="nav-item mb-2">
+                    <a href="#" class="nav-link p-0 text-body-secondary">
+                        Recipe title 3
+                    </a>
+                </li>
+                <li class="nav-item mb-2">
+                    <a href="#" class="nav-link p-0 text-body-secondary">
+                        Recipe title 4
+                    </a>
+                </li>
+                <li class="nav-item mb-2">
+                    <a href="#" class="nav-link p-0 text-body-secondary">
+                        Recipe title 5
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="col mb-3">
+            <h5>Useful Links</h5>
+            <ul class="nav flex-column">
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Search
+                        Categories</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Search
+                        Recipes</a></li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">Most Viewed
+                        Recipe</a></li>
+                <li class="nav-item mb-2"><a href="faq.html" class="nav-link p-0 text-body-secondary">FAQs</a>
+                </li>
+                <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-body-secondary">About</a></li>
+            </ul>
+        </div>
+    </div>
+    <ul class="nav justify-content-center border-bottom border-top mt-2">
+        <li class="nav-item"><a href="terms.html" class="nav-link px-2 text-body-secondary">Terms &
+                Conditions</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Privacy Policy</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Photo Usage Policy</a></li>
+    </ul>
+    <div class="row">
+        <div class="col-md-6 col-12 mc-color-gray">
+            Developed with 🤩️ by <a rel="nofollow" target="_parent" href="#!" class="mc-external-link">Put Your
+                Name
+                Here Dear Laravel Developer</a>
+        </div>
+        <div class="col-md-6 col-12 mc-copyright">
+            <p class="text-body-secondary">© 2023 Multicaret, Inc</p>
+        </div>
+    </div>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,65 @@
+<header class="mc-header" id="mc-header">
+    <div class="mc-header-wrapper">
+        <button class="navbar-toggler" type="button" aria-label="Toggle navigation">
+            <i class="fas fa-bars"></i>
+        </button>
+        <div class="mc-site-header">
+            <a href="index.html" class="mb-3 mx-auto site-logo">
+                <img src="img/logo.png" alt="Logo" width="70px">
+            </a>
+            <h1 class="text-center site-title">Lara<span class="fw-light">Recipes</span></h1>
+        </div>
+        <nav class="mc-nav" id="mc-nav">
+            <ul>
+                <li class="mc-nav-item active"><a href="index.html" class="mc-nav-link">
+                        <i class="fas fa-carrot"></i>
+                        Home
+                    </a></li>
+                <li class="mc-nav-item"><a href="categories.html" class="mc-nav-link">
+                        <i class="fas fa-cubes-stacked"></i>
+                        Categories
+                    </a></li>
+                <li class="mc-nav-item"><a href="single-category.html" class="mc-nav-link">
+                        <i class="fas fa-cubes-stacked"></i>
+                        Single Category
+                    </a></li>
+                <li class="mc-nav-item"><a href="single-recipe.html" class="mc-nav-link">
+                        <i class="fas fa-bowl-food"></i>
+                        Single Recipe
+                    </a></li>
+                <li class="mc-nav-item"><a href="about.html" class="mc-nav-link">
+                        <i class="fas fa-lemon"></i>
+                        About Us
+                    </a></li>
+                <li class="mc-nav-item"><a href="contact.html" class="mc-nav-link">
+                        <i class="fas fa-utensils"></i>
+                        Contact Us
+                    </a></li>
+            </ul>
+        </nav>
+        <div class="mc-mb-65">
+            <a href="https://x.com/mkwsra" class="mc-social-link">
+                <i class="fab fa-x-twitter mc-social-icon"></i>
+            </a>
+            <a href="https://linkedin.com/in/mkwsra" class="mc-social-link">
+                <i class="fab fa-linkedin-in mc-social-icon"></i>
+            </a>
+            <a rel="nofollow" href="https://fb.com/mkwsra" class="mc-social-link">
+                <i class="fab fa-facebook-f mc-social-icon"></i>
+            </a>
+            <a href="https://instagram.com/multicaret" class="mc-social-link">
+                <i class="fab fa-instagram mc-social-icon"></i>
+            </a>
+        </div>
+        <p class="pr-5 text-white">
+            Lara Recipes a demo of a Laravel tutorial created by
+            <a href="https://mkwsra.com" class="text-dark">Mo Kawsara</a>
+            using the Xtra Blog html5 theme as a
+            foundation, this theme got tweaked for the purposes of this tutorial.
+        </p>
+        <p class="mc-mb-80 text-white">If you liked this tutorial, consider helping me out by sharing this video series
+            with others and perhaps by
+            liking my <a href="https://www.youtube.com/@multi-caret" target="_blank" class="text-dark">YouTube
+                videos</a></p>
+    </div>
+</header>
