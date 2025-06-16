8a23d12976855ebc5ef297ac6eda51b231e0b17a
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-06-16 05:29:15 +0200

    add Model, migration, seeders and layout view

--- database/factories/RecipeFactory.php
+++ database/factories/RecipeFactory.php
@@ -17,7 +17,28 @@ class RecipeFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'title' => fake()->sentence(mt_rand(3, 8)),
+            'excerpt' => fake()->text(),
+            'instructions' => fake()->realText(400),
+            'description' => fake()->realText(800),
+            'notes' => fake()->realText(),
+            'prepare_time' => fake()->randomElement([0, 5, 10, 15, 20, 25, 30]),
+            'cooking_time' => fake()->randomElement([15, 30, 45, 60, 75, 90]),
+            'servings' => fake()->randomElement([2, 4, 6]),
+            'youtube_url' => fake()->randomElement([
+                'https://www.youtube.com/watch?v=P6W8kwmwcno',
+                'https://www.youtube.com/watch?v=6LDswBUN21o',
+                'https://www.youtube.com/watch?v=xniS7kMpW4I',
+                'https://www.youtube.com/watch?v=pw14NzfYPa8',
+                'https://www.youtube.com/watch?v=BHcyuzXRqLs'
+            ]),
+            'featured_at' => fake()->randomElement([true, false])? now(): null,
+            'is_low_carb' => fake()->randomElement([true, false]),
+            'is_high_protein' => fake()->randomElement([true, false]),
+            'is_spicy' => fake()->randomElement([true, false]),
+            'is_vegetarian' => fake()->randomElement([true, false]),
+            'is_vegan' => fake()->randomElement([true, false]),
+            'is_pescatarian' => fake()->randomElement([true, false]),
         ];
     }
 }
---
+++ database/migrations/2025_06_15_181750_create_recipes_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('excerpt')->nullable();
+            $table->longText('instructions')->nullable();
+            $table->longText('description')->nullable();
+            $table->string('notes')->nullable();
+            $table->unsignedSmallInteger('prepare_time')->nullable()->comment('Measured by minutes');
+            $table->unsignedSmallInteger('cooking_time')->nullable()->comment('Measured by minutes');
+            $table->string('servings')->nullable();
+            $table->string('youtube_url')->nullable();
+            //region Flags
+            $table->boolean('is_low_carb')->default(false);
+            $table->boolean('is_high_protein')->default(false);
+            $table->boolean('is_spicy')->default(false);
+            $table->boolean('is_vegetarian')->default(false);
+            $table->boolean('is_vegan')->default(false);
+            $table->boolean('is_pescatarian')->default(false);
+            //endregion
+            $table->unsignedBigInteger('view_count')->default(1);
+            $table->timestamp('featured_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('recipes');
+    }
+};
