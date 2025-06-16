e8b0498ef449eed76aa4e29197541ac153818e36
MateuszKuruc <105269423+MateuszKuruc@users.noreply.github.com>
2025-06-16 12:41:39 +0200

    make homepage dynamic with card components and categories

--- resources/views/components/recipe-card-sm.blade.php
+++ resources/views/components/recipe-card-sm.blade.php
@@ -1,6 +1,7 @@
 @props([
     /** @var App\Models\Recipe */
-    'latestRecipe'
+    'latestRecipe',
+    'isFeatured'
 ])
 
 <article {{ $attributes->class(['col-12 col-md-6 col-lg-3']) }}>
@@ -8,6 +9,13 @@
         <div class="mc-post-link-inner">
             <img src="img/{{ mt_rand(1,6) }}.jpeg" alt="{{ $latestRecipe->title }}" class="img-fluid">
         </div>
+        @if(isset($isFeatured) && $isFeatured)
+        <span class="position-absolute mc-new-badge">
+            <i class="fas fa-certificate">
+                Featured
+            </i>
+        </span>
+        @endif
         <h2 class="mc-pt-30 mc-post-title">{{ $latestRecipe->title }}</h2>
     </a>
     <div class="d-flex justify-content-between">
